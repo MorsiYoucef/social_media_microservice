a310ce04f32f557a19ee311bba527b61b9749f8b
Morsi Youcef <141324213+MorsiYoucef@users.noreply.github.com>
2025-05-29 07:04:03 +0100

    feature: adding middleware to log requests and add timestamps

---
+++ src/middlewares/custom.middleware.ts
@@ -0,0 +1,15 @@
+import { Request, Response, NextFunction } from 'express';
+
+export const requestLogger = (req: Request, res: Response, next: NextFunction) => {
+    const timestamp = new Date().toISOString();
+    const method = req.method;
+    const url = req.url;
+    const userAgent = req.get('User-Agent');
+    console.log(`[${timestamp}] ${method} request to ${url} from ${userAgent}`);
+    next();
+}
+
+export const addTimesStamp = (req: Request, res: Response, next: NextFunction) => {
+    const timeStamp = new Date().toISOString();
+    next();
+}
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,19 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { configureCors } from './config/cors.config';
+import { addTimesStamp, requestLogger } from './middlewares/custom.middleware';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(requestLogger);
+app.use(addTimesStamp);
+
 app.use(express.json());
 app.use(configureCors());
+
 // app.use(cors());
 
 
