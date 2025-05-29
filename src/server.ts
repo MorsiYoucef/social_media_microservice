73e6bb1842db422c3b6977a396b4f2f05cf6a792
Morsi Youcef <141324213+MorsiYoucef@users.noreply.github.com>
2025-05-29 10:21:41 +0100

    feature: adding global error handler and versioning middleware

--- src/server.ts
+++ src/server.ts
@@ -2,19 +2,27 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { configureCors } from './config/cors.config';
 import { addTimesStamp, requestLogger } from './middlewares/custom.middleware';
+import { globalErrorHandler } from './middlewares/errorHandler';
+import { urlVersioning } from './middlewares/apiVersionning';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Middleware to log requests and add timestamps
+//Why Use It:
+//For debugging: Helps you see what’s happening in real time
+//For monitoring: Understand how users are interacting with your server
 app.use(requestLogger);
 app.use(addTimesStamp);
 
 app.use(express.json());
 app.use(configureCors());
 
-// app.use(cors());
+app.use('/api/v1', urlVersioning('v1') )
+
+app.use(globalErrorHandler);
 
 
 app.listen(PORT, () => {
